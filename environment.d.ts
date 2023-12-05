a513d6b0129c23e8b96c4d109c9f618134dd8e9c
Kenny Nguyen <kenny@Kennys-Laptop.local>
2023-12-05 12:03:04 -0800

    remove: env.d.ts

--- environment.d.ts
+++ environment.d.ts
@@ -2,14 +2,9 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       MONGO_URI: string;
-      GITHUB_AUTH_TOKEN: string;
       NODE_ENV: "development" | "production";
-      PORT?: string;
-      PWD: string;
     }
   }
 }
 
-// If this file has no import/export statements (i.e. is a script)
-// convert it into a module by adding an empty export statement.
 export {};
