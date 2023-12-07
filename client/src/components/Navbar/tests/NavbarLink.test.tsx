c839aa9bbcad65ee80b9d54d766896d62398abf9
Kenny Nguyen <kenny@Kennys-Laptop.local>
2023-12-07 12:14:29 -0800

    test: NavbarLink

--- client/src/components/Navbar/tests/NavbarLink.test.tsx
+++ client/src/components/Navbar/tests/NavbarLink.test.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-
+import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { NavbarLink } from '../NavbarLink';
 
-test('NavbarLink', () => {
-  beforeAll(() => {
-    render(
-      <MemoryRouter>
-        <NavbarLink to="/home">Home</NavbarLink>
-      </MemoryRouter>
-    );
-  });
+describe('NavbarLink', () => {
+  it('renders the link with the correct href and children', () => {
+    const testText = 'Test Link';
+    const testHref = '/test-route';
 
-  expect(screen.findByRole('navigation')).toBeInTheDocument();
-  expect(screen.getByText('Home')).toBeInTheDocument();
+    render(
+      <BrowserRouter>
+        <NavbarLink to={testHref}>{testText}</NavbarLink>
+      </BrowserRouter>
+    );
+
+    const linkElement = screen.getByRole('navigation');
+    expect(linkElement).toHaveTextContent(testText);
+    expect(linkElement.getAttribute('href')).toBe(testHref);
+  });
 });
